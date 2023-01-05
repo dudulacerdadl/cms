a96b8e6c94ecb76f73c2983a5f37c011df1aec73
Eduardo Lacerda <eduardo.filho@bistwobis.com.br>
2023-01-05 13:34:43 -0300

    :sparkles: Added function for operations - MARCOS

--- lib/Connection.php
+++ lib/Connection.php
@@ -1,6 +1,10 @@
 <?php
 
 require_once ROOT . '/lib/Env.php';
+require_once ROOT . '/lib/db/MySQL/Delete.php';
+require_once ROOT . '/lib/db/MySQL/Insert.php';
+require_once ROOT . '/lib/db/MySQL/Select.php';
+require_once ROOT . '/lib/db/MySQL/Update.php';
 
 class Connection
 {
@@ -11,12 +15,12 @@ class Connection
 
         try {
             return new PDO(
-                "mysql:host=".$env['HOST'].";dbname=".$env['DBNAME'],
+                "mysql:host=" . $env['HOST'] . ";dbname=" . $env['DBNAME'],
                 $env['USERNAME'],
                 $env['PASSWORD']
             );
         } catch (PDOException $pe) {
-            die("Não foi possível se conectar ao banco de dados ".$env['DBNAME']." :" . $pe->getMessage());
+            die("Não foi possível se conectar ao banco de dados " . $env['DBNAME'] . " :" . $pe->getMessage());
         }
     }
 
@@ -26,9 +30,49 @@ class Connection
         $env = $env->getVariables();
 
         try {
-            return new PDO('sqlite:'.ROOT.'/Resource/db/Banco.db');
+            return new PDO('sqlite:' . ROOT . '/Resource/db/Banco.db');
         } catch (PDOException $pe) {
-            die("Não foi possível se conectar ao banco de dados ".$env['DBNAME']." :" . $pe->getMessage());
+            die("Não foi possível se conectar ao banco de dados " . $env['DBNAME'] . " :" . $pe->getMessage());
+        }
+    }
+
+    /**
+     * @param $name
+     * @param array   $params
+     */
+    public static function operation(
+        $name,
+        array $params
+    ) {
+        switch (strtolower($name)) {
+            case 'delete':
+                return new \Delete(
+                    $params['table'],
+                    $params['id'],
+                );
+            case 'insert':
+                return new \Insert(
+                    $params['table'],
+                    $params['params'],
+                    $params['values'],
+                );
+            case 'select':
+                return new \Select(
+                    $params['table'],
+                    $params['params'],
+                    $params['where'],
+                    $params['whereParams'],
+                    $params['whereValues']
+                );
+            case 'update':
+                return new \Update(
+                    $params['table'],
+                    $params['id'],
+                    $params['params'],
+                    $params['values']
+                );
+            default:
+                throw new Exception("Operation invalid");
         }
     }
 }
